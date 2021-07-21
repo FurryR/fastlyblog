5de71dda5f8196b03e86abdf61e1b0bd0bc55cba
Fox Awa <awa_the_fox@qq.com>
2021-07-21 22:43:25 +0800

    Add files via upload

--- blog/1/init.js
+++ blog/1/init.js
@@ -1,1 +1,15 @@
-
+//init
+clearContent();
+//metadata start
+$title("Hello World");
+$info("断幺九", 1626870667100);
+//metadata end
+var content = document.getElementById("content");
+const cont=`
+<p>这是第一篇博客。</p>
+<p>Javascript 博客描述API</p>
+<p>clearContent() : 删除画面内容。</p>
+<p>$title(title) : 设定画面标题。</p>
+<p>$info(author,timestamp) : 设定文章的作者和作成时间。</p>
+`;
+content.innerHTML+=cont;
